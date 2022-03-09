e1ead788fcaf45b41ed4054ce9523b84855aebd7
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-03-09 12:39:07 +0200

    Ubuntu 20.04 Clang 11 Reparat erori

--- main.cpp
+++ main.cpp
@@ -85,7 +85,7 @@ int main() {
     Nava nava3 = nava1;
     nava2 = nava3 = nava1;
     Port port{ro, "Constanta"};
-    Port port{ro, "Braila"};
+    Port port2{ro, "Braila"};
     std::cout << nava1;
     return 0;
 }
