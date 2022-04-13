63f7b1da9f9f4225dcdd5bb47649fabe1776e6b8
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-04-13 12:46:37 +0300

    Schimbat cod in functie de: Ar ajuta aici un comentariu // descarca marfa

--- main.cpp
+++ main.cpp
@@ -113,10 +113,12 @@ public:
 
     void intra_port(const Port &port_destinatie) {
         int n = int(this->marfuri.size() - 1);
+        //Descarca marfa care are ca destinatie portul in care intra
         for(int i = 0; i <= n; ++i) {
             if (this->marfuri[i].getDestinatie() == port_destinatie) {
                 this->marfuri[i] = this->marfuri[n];
                 n--;
+                i--;//Deoarece ce am pus pe pozitia i poate sa fie tot pt portul acesta
             }
         }
         this->port = port_destinatie;
