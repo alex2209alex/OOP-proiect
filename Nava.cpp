0e10e75ebc74c6076027f6296a044d34ad30f08d
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-05-11 17:37:14 +0300

    ideea cu git e să nu ai nevoie de așa ceva pt că te poți uita pe istoric când ai nevoie; ai ține comentarii cu codul vechi doar dacă îl comentezi temporar sau în alte situații rare -s a rezolvat deja inainte
    mai degrabă ai face un enum dacă e să semene cu un automat și ai pune definiția să fie publică și nu mai ai nevoie de getteri
    e cam dubios cu acești getteri pt că întorci niște constante a căror valoare nu ar avea de ce să difere de la un obiect la altul; getterii de obicei ar întoarce date specifice unui obiect
    -rezolvat altfel

--- Nava.cpp
+++ Nava.cpp
@@ -8,7 +8,7 @@
 Nava::Nava(const Port &portCurent, const std::string &denumire, const Tara &pavilion, int imoNumber) : portCurent(portCurent), denumire(denumire), pavilion(pavilion), imoNumber(imoNumber) {}
 Nava::Nava(const Nava& other) : portCurent(other.portCurent), denumire(other.denumire), pavilion(other.pavilion), imoNumber(other.imoNumber) {}
 
-void Nava::intrare_port(const Port &port) {
+void Nava::intrare_port(const Port &port) { // 0 inseamna ca portul e inchis, 1 ca e deschis
     if(this->portCurent.getStarePort() == 0) {
         std::cout << "Portul de plecare e inchis\n";
     }
