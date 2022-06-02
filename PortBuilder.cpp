6be8024c7ec39d42ea045d2298b2c6de14c08b98
Alexandru Pavel <alex.pavel2002@gmail.com>
2022-06-02 15:18:25 +0300

    Am terminat

--- PortBuilder.cpp
+++ PortBuilder.cpp
@@ -26,7 +26,7 @@ Port PortBuilder::build() {
     if(p.starePort != Port::DESCHIS && p.starePort != Port::INCHIS) {
         throw build_object_invalid();
     }
-    if(p.tara.getNume().empty() or p.tara.getContinent().empty() or p.oras.empty()) {
+    if(p.tara.getNume().empty() or p.tara.getContinent().empty() or p.oras.empty() or p.tara.getIdentificator().empty()) {
         throw build_object_invalid();
     }
     auto p2 = p;
